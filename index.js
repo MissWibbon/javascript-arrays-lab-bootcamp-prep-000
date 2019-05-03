e74d9b7e659cd68eff5beb9bf572d026e36efa58
Michelle Wuebben <wuebbenmichelle@gmail.com>
2019-05-03 01:15:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function destructivelyAppendKitten(name){
   return kittens
 }
 
+function destructivelyPrependKitten(){
+  kittens.unshift()
+}
+
 function destructivelyRemoveFirstKitten(){
   return kittens.shift()
 }
